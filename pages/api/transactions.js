b35d440f2015925cd21237a8067a066b3baf4b88
mamun01764 <113938307+mamun01764@users.noreply.github.com>
2026-02-23 11:28:03 +0600

    Update transactions.js

--- pages/api/transactions.js
+++ pages/api/transactions.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(
       `https://deep-index.moralis.io/api/v2.2/${address}?chain=base&limit=20`,
-      { headers: { 'X-API-Key': process.env.NEXT_PUBLIC_MORALIS_API_KEY } }
+      { headers: { 'X-API-Key': process.env.MORALIS_API_KEY } }
     );
     const data = await response.json();
     res.status(200).json(data.result || []);

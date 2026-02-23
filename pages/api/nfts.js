0fcde46bc112cf1798e835ee05e8d615d250d218
mamun01764 <113938307+mamun01764@users.noreply.github.com>
2026-02-23 11:28:53 +0600

    Update nfts.js

--- pages/api/nfts.js
+++ pages/api/nfts.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(
       `https://deep-index.moralis.io/api/v2.2/${address}/nft?chain=base&limit=12`,
-      { headers: { 'X-API-Key': process.env.NEXT_PUBLIC_MORALIS_API_KEY } }
+      { headers: { 'X-API-Key': process.env.MORALIS_API_KEY } }
     );
     const data = await response.json();
     res.status(200).json(data.result || []);
